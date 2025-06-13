bd595ecda5a3ffb05452fcff1d8a493a3125931e
Enderic <133310359+Enderic@users.noreply.github.com>
2025-06-12 19:47:44 -0500

    Rc<T> section done

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,41 @@
 /*
-    Drop trait lets you customize what happens when a value is gonna go out of scope
-        - Box<T> deallocates space on heap when it's dropped
+    Sometimes an owner might have multiple owners, like when multiple edges in a graph point to same vertex
+    We enable multiple ownership by using Rc<T> (Reference Counting), which keeps track of the numer of references
+        - If there's 0 references, value is cleaned up
+    Only for use in single-threaded scenarios
+    Also for read-only (immutable references)
 */
 
-use std::mem::drop;
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+use std::rc::Rc;
+use crate::List::{Cons, Nil};
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
-    drop(c); // Can't do c.drop()
-    println!("C CSP was dropped.");
-    // When c & d go out of scope (without the drop function doing anything), they go in reverse order of their initialization because they're on the stack like in c++
-    // W/ the drop of c, c will drop before d now
+    /*
+        Assume we're using Box<T> instad of Rc<T> for this scenario
+
+        let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+        let b = Cons(3, Box::new(a));
+        let c = Cons(4, Box::new(a));
+        
+        This errors b/c Cons own data they hold, so when b is created, a is moved into b, & c can't use it
+        We could specify lifetime parameters to say that every element inthe list lives as long as the entire list, but this isn't alwasy true
+    */
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a)); 
+    let b = Cons(3, Rc::clone(&a)); // We do a clone of the Rc<List> a (normal .clone() also works)
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        // When c goes out of scope, the count decreases
+        // The Drop trait does the decrease for us
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
 }
