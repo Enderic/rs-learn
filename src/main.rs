9d0e5c7d67a8e58bb8723e0284d6d7878bf2ab63
Enderic <133310359+Enderic@users.noreply.github.com>
2025-06-14 16:21:16 -0500

    weak<T> part 1

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,35 @@
-use crate::List::{Cons, Nil};
+/*
+    With Rc<T>, we can create weak references by calling Rc::downgrade(), which returns smart pointer Weak<T>
+        - Weak references don't express ownership
+        - Is kept track of by weak_count (doesn't need to be 0 for cleanup)
+
+    Because the value Weak<T> references might've been dropped, we use Rc::upgrade() which returns an Option<Rc<T>>
+*/
+
 use std::cell::RefCell;
 use std::rc::Rc;
 
 #[derive(Debug)]
-enum List {
-    // i32 is data, RefCell<Rc<List>> is practically "next"
-    Cons(i32, RefCell<Rc<List>>),
-    Nil,
-}
-
-impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match self {
-            Cons(_, item) => Some(item),
-            Nil => None,
-        }
-    }
+struct Node {
+    value: i32,
+    /*
+        We want the node to contain its children nodes (Node)
+        We want the children node to have ownership shared, like if we want a node to have multiple parents (Rc<Node>)
+        We can have multiple children (Vec<Rc<Node>>)
+        We want to be able to mutate even on immutable Nodes (RefCell<Vec<Rc<Node>>>)
+    */
+    children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn main() {
-    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil)))); // We create an Rc<List> of a
+    // branch knows about leaf, but leaf doesn't know about branch, which we want to happen
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
 
-    println!("a initial rc count = {}", Rc::strong_count(&a)); // There's 1 reference to a, so it prints 1
-    println!("a next item = {:?}", a.tail());
-
-    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a)))); // Create Rc<List> holding another List by cloning a
-
-    println!("a rc count after b creation = {}", Rc::strong_count(&a)); // We have another reference to a's data on heap, so 2
-    println!("b initial rc count = {}", Rc::strong_count(&b)); // For b, just the initialization so just 1
-    println!("b next item = {:?}", b.tail()); // It goes b->a->Nil
-
-    if let Some(link) = a.tail() {
-        // When we get here, we first dereference the reference, then we do borrow_mut() to get the Rc<List>
-        // Then we set the next to b, turning it into a cycle a <=> b
-        *link.borrow_mut() = Rc::clone(&b);
-    }
-
-    println!("b rc count after changing a = {}", Rc::strong_count(&b));
-    println!("a rc count after changing a = {}", Rc::strong_count(&a));
-
-    // Uncomment the next line to see that we have a cycle;
-    // it will overflow the stack.
-    // println!("a next item = {:?}", a.tail());
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
 }
