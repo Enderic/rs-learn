ceeddeaa99b888dfd97dfcb97ec461e02bbf569a
Enderic <133310359+Enderic@users.noreply.github.com>
2025-06-18 12:07:57 -0500

    test change

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,25 @@
-use trpl::{Either, Html};
-
-// Super Basic Web Scraper!
+use std::time::Duration;
+use std::pin::{Pin, pin};
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    println!("Starting...");
 
     trpl::run(async {
-        // Call page_title() for each url to get futures
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
+        let slow = async {
+            println!("'slow' started.");
+            trpl::sleep(Duration::from_millis(100)).await;
+            println!("'slow' finished.");
+        };
 
-        // We pass the futures into ::race() to see which finishes first
-        // Runs it for us
-        // We would usually use ::select() more
-        // It returns an Either to show which finished first
-        let (url, maybe_title) =
-            match trpl::race(title_fut_1, title_fut_2).await {
-                Either::Left(left) => left,
-                Either::Right(right) => right,
-            };
+        let fast = async {
+            println!("'fast' started.");
+            trpl::sleep(Duration::from_millis(50)).await;
+            println!("'fast' finished.");
+        };
 
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title is: '{title}'"),
-            None => println!("Its title could not be parsed."),
-        }
-    })
-}
+        
+        trpl::race(slow, fast).await;
+    });
 
-// Changed return type to include original url
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let text = trpl::get(url).await.text().await;
-    let title = Html::parse(&text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
+    println!("Back to sync code");
 }
