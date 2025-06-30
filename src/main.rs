01241816453cf7a239452f8ebfcb886e6e3e06f7
Enderic <mahiyan21@gmail.com>
2025-06-30 11:11:51 -0500

    advanced traits p1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,60 @@
-// You can define a trait as unsafe if the compiler can't verify all safety guarantees
-unsafe trait MyTrait {
-    // method signatures here
+/*
+    When using generic type parameters, we can specify a default concrete type for it
+    In this case, we implement the Add trait to our Point struct to add two points together
+*/
+
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-// Implementing an unsafe trait requires `unsafe impl`
-// You're taking manual responsibility for upholding invariants
-unsafe impl MyTrait for i32 {
-    // method implementations
+impl Add for Point {
+    type Output = Point;
+
+    // Still need to explicitly define the type of the parameter
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
 
-// If we have a type A that has a type B that doesn't implement Send or Sync, like raw pointers, & we want to mark type A as Send or Sync, we must use unsafe
-// Rust doesn't know if type B upholds safety rules regarding read/write from multiple threads, so we do it manually w/ unsafe
+// This thin wrapping of an existing type (Meters/Millimeters wrapping over u32) is called `newtype pattern`
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    f
+}
+
+// Add implementation
+// Rhs=Self; Rhs is a generic & Self is the default concrete type
+// This is saying that the default type of Rhs will be the type we're implementing Add on
+// In the point implementation, we do use the default for Rhs
+// In the meters implementation, we add millimeters w/ meters, not using the default for Rhs
+trait Add<Rhs=Self> {
+    type Output;
+
+    fn add(self, rhs: Rhs) -> Self::Output;
+}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+}
