5e2d5e8fe2759d9beeb1f412adb7ab3e6e20712e
Enderic <133310359+Enderic@users.noreply.github.com>
2025-06-30 09:04:33 -0500

    last part of unsafe section

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-// We can also use extern to let other languages call rust functions
-// On a function, we add `extern` & specify the ABI before `fn`
-// We also add `[unsafe(no_mangle)]` to not mangle the name of the function
-// Mangling is when compiler changes name of function to a different name
-// Language compilers do this & differently per language, so we do this to disable it since there could e name collisions
-// This is also unsafe so we have to mark it unsafe
-
-#[unsafe(no_mangle)]
-pub extern "C" fn call_from_c() {
-    println!("Just called a Rust function from C!");
+// You can define a trait as unsafe if the compiler can't verify all safety guarantees
+unsafe trait MyTrait {
+    // method signatures here
 }
+
+// Implementing an unsafe trait requires `unsafe impl`
+// You're taking manual responsibility for upholding invariants
+unsafe impl MyTrait for i32 {
+    // method implementations
+}
+
+// If we have a type A that has a type B that doesn't implement Send or Sync, like raw pointers, & we want to mark type A as Send or Sync, we must use unsafe
+// Rust doesn't know if type B upholds safety rules regarding read/write from multiple threads, so we do it manually w/ unsafe
