5857fbc1a914c7566adfba50c9028ac3878b6067
Enderic <133310359+Enderic@users.noreply.github.com>
2025-07-02 14:25:26 -0500

    never type

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,39 @@
 /*
-    We can use the newtype pattern in a lot of ways
-    In the previous section we had the Millimeter & Meter structs that wrapped a u32 in a newtype (tuple struct)
-    If we try writing a function that takes a Millimeter type, program wouldn't compile if we tried to use Meters or u32
-
-    Rust has type aliases where we can give a name for an existing type w/ the `type` keyword
-    Treated the same as the the original type
-    Only downside is we don't get type checking between the two, so if we mix them up, we don't get an error
+    Rust has the ! type that's called the never type since it has no value
+    Below is a function that returns this never type
 */
-
-type Kilometers = i32;
-
-// Main use case is to reduce repetition of long types
-// Say we have the function below, we might wanna use that type in a lot of places, which can get confusing & repetitive
-fn takes_long_type(f: Box<dyn Fn() + Send + 'static>) {}
-
-// If we put it in a type alias, then all we need to do is use Thunk, making everything more manageable
-type Thunk = Box<dyn Fn() + Send + 'static>;
-
-fn takes_short_type(f: Thunk) {}
-
-// We can also use it w/ Result<T, E> which is a common pattern that can get annoying to type out fully
-// We can use ? on this as well like we do w/ regular Result types since it's treated as the same
-type Res<T> = std::result::Result<T, std::io::Error>;
-
-pub trait Writing {
-    fn write(&mut self, buf: &[u8]) -> Res<usize>;
-    fn flush(&mut self) -> Res<()>;
+fn bar() -> ! {
+    panic!("This will always panic!")
 }
 
+// From the guessing game we had this code
+// Each arm must return the same type, so if Err(_) arm returned string, it wouldn't compile
+// This works because continue returns !, Rust looks at both types, & sees continue (!) & u32
+// Since ! can't ever have a value, it says guess is a u32
+// The ! type can work w/ any other type
+let guess: u32 = match guess.trim().parse() {
+    Ok(num) => num,
+    Err(_) => continue,
+};
 
+// Implementation for Option<T>
+// The never type is also useful w/ panic! macro as well
+// In this case the same thing happens, the match sees two types, T & panic! which is a ! type, so the result of the match is of type T, making this work as well
+impl<T> Optionn<T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            Some(val) => val,
+            None => panic!("called `Option::unwrap()` on a `None` value"),
+        }
+    }
+}
+
+// A loop also is also of type !
+// Since the loop doesn't end, it's of type !, but if it did have a break or ends in some way, this wouldn't be the case
 fn main() {
+    print!("forever");
 
-    let x: Kilometers = 6;
-    let mut y = 1;
-
-    // Can add together
-    let mut z = x + y;
-    println!("x + y = {}", x + y);
-
-    // Pretty much treated like the same variable
-    let mut a = &mut y;
-    *a += 1;
-    a = &mut z;
-    *a = *a + x;
-    println!("y, z ({}, {})", y, z);
+    loop {
+        print!("and ever");
+    }
 }
